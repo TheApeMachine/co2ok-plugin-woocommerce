46eb13714f2e58fce349cf637be14d314824674c
Milo de Vries <intensmilo@gmail.com>
2017-08-10 14:57:38 +0200

    Increases version to test GH updater

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -6,7 +6,7 @@
  *
  * Plugin URI: https://github.com/Mil0dV/co2ok-plugin-woocommerce
  * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
- * Version: 0.1
+ * Version: 0.1.1
  *         (Remember to change the VERSION constant, below, as well!)
  * Author: Milo de Vries
  * Author URI: http://www.co2ok.eco/
