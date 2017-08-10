189ad88f3a9062dd5534f1a57ba34465e0d53ff0
Milo de Vries <intensmilo@gmail.com>
2017-08-10 13:50:31 +0200

    Adds minimal WC plugin implementation

---
+++ co2ok-plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: CO2ok WooCommerce Plugin
+ *
+ * Description: A WooCommerce plugin to integrate CO2ok
+ *
+ * Plugin URI: https://github.com/Mil0dV/co2ok-plugin-woocommerce
+ * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
+ * Version: 0.1
+ *         (Remember to change the VERSION constant, below, as well!)
+ * Author: Milo de Vries
+ * Author URI: http://www.co2ok.eco/
+ * License: GPLv2
+ * @package co2ok-plugin-woocommerce
+ *
+ */
+
+/**
+ * Prevent data leaks
+*/
+if ( ! defined( 'ABSPATH' ) ) { 
+    exit; // Exit if accessed directly
+}
+
+ /**
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+  echo 'Hello World';
+}
+?>
