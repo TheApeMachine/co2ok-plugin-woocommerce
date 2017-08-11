45f621f427eed059a2a0308e79dc7b90f5ece449
Milo de Vries <intensmilo@gmail.com>
2017-08-11 11:42:26 +0200

    Adds compensation field to checkout

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -6,7 +6,7 @@
  *
  * Plugin URI: https://github.com/Mil0dV/co2ok-plugin-woocommerce
  * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
- * Version: 0.1.2
+ * Version: 0.1.3
  *         (Remember to change the VERSION constant, below, as well!)
  * Author: Milo de Vries
  * Author URI: http://www.co2ok.eco/
@@ -26,13 +26,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Check if WooCommerce is active
  **/
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-  // Hook in
-  add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+    // Hook in
+    add_filter( 'woocommerce_checkout_fields', 'custom_override_checkout_fields'
+    );
 
-  // Our hooked in function - $fields is passed via the filter!
-  function custom_override_checkout_fields( $fields ) {
-      $fields['order']['order_comments']['placeholder'] = 'Wahooo!';
-      return $fields;
-  }
+    // Our hooked in function - $fields is passed via the filter!
+    function custom_override_checkout_fields( $fields ) {
+        $fields['billing']['co2ok_compensation_checkbox']['options'] = array(
+            'option_1' => 'compensate',
+            'option_2' => "don't compensate"
+        );
+        return $fields;
+    }
 }
 ?>
