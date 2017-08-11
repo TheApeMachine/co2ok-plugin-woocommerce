7cce29dc3ef39eea8e49d8e074eb2a5e0b33818c
Milo de Vries <intensmilo@gmail.com>
2017-08-11 17:54:41 +0200

    Adds checkbox to review order page

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -6,7 +6,7 @@
  *
  * Plugin URI: https://github.com/Mil0dV/co2ok-plugin-woocommerce
  * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
- * Version: 0.1.3
+ * Version: 0.1.4
  *         (Remember to change the VERSION constant, below, as well!)
  * Author: Milo de Vries
  * Author URI: http://www.co2ok.eco/
@@ -16,27 +16,33 @@
  */
 
 /**
+ * This plugin's version
+ */
+const VERSION = '0.1.4';
+
+/**
  * Prevent data leaks
 */
 if ( ! defined( 'ABSPATH' ) ) { 
     exit; // Exit if accessed directly
 }
 
- /**
+/**
  * Check if WooCommerce is active
  **/
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-    // Hook in
-    add_filter( 'woocommerce_checkout_fields', 'custom_override_checkout_fields'
-    );
 
-    // Our hooked in function - $fields is passed via the filter!
-    function custom_override_checkout_fields( $fields ) {
-        $fields['billing']['co2ok_compensation_checkbox']['options'] = array(
-            'option_1' => 'compensate',
-            'option_2' => "don't compensate"
-        );
-        return $fields;
+    add_action( 'woocommerce_review_order_after_shipping', 'co2ok_compensation');
+
+    $checked = '';
+    function co2ok_compensation() { echo '<tr id="carbon-item">
+        <th>'.__('Carbon Offset','woocommerce').'<a class="question-mark tip" href="http://co2ok.eco" target="_blank">
+        <span>Cool story about this offsetting stuff.</span>
+        </a></th>
+        <td><input type="checkbox" name="co2-ok" id="co2-ok" '.$checked.' /></td>
+    </tr>';
+
+    echo '</td></tr>';
     }
 }
 ?>
