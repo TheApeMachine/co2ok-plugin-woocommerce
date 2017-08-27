53fd5846f357286e2c529c48e1d4f41d084bfc10
root <root@localhost.localdomain>
2017-08-27 18:42:59 -0400

    Cart + Checkout checkbox in working order, still alot to clean up...

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -8,7 +8,10 @@
  * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
  * Version: 0.1.6
  *         (Remember to change the VERSION constant, below, as well!)
- * Author: Milo de Vries
+ * Author:
+ * Chris Fuller,
+ * Milo de Vries
+ *
  * Author URI: http://www.co2ok.eco/
  * License: GPLv2
  * @package co2ok-plugin-woocommerce
@@ -21,6 +24,19 @@ class Co2ok_Plugin
      * This plugin's version
      */
     const VERSION = '0.1.6';
+
+    private $percentage = 0.0165;
+
+    //This function is called when the user activates the plugin.
+    static function Activated()
+    {
+
+    }
+    //This function is called when the user activates the plugin.
+    static function Deactivated()
+    {
+
+    }
 
     /**
      * Constructor.
@@ -40,30 +56,101 @@ class Co2ok_Plugin
         if ( in_array( 'woocommerce/woocommerce.php', apply_filters(
             'active_plugins', get_option( 'active_plugins' ) ) ) )
         {
-            add_action( 'woocommerce_after_order_notes', array($this,'co2ok_compensation'));
-            add_action( 'woocommerce_cart_calculate_fees',array($this,'woocommerce_custom_surcharge'));
+            add_action('woocommerce_after_order_notes' ,array($this,'my_custom_checkout_field') );
+            add_action('woocommerce_cart_calculate_fees', array($this,'woocommerce_custom_surcharge'));
+            add_action('woocommerce_cart_collaterals' , array($this,'my_custom_cart_field'));
         }
     }
 
-    public function co2ok_compensation()
-    {
-        echo '<tr id="carbon-item">
-        <input type="checkbox" name="co2-ok" id="co2-ok" />'.__(' Maak mijn
-        aankoop CO₂ok voor €0,23','woocommerce').'<a class="question-mark tip"
-        href="http://co2ok.eco" target="_blank">
-        <span>wut?</span></a></tr>';
-    }
-
-    public function woocommerce_custom_surcharge()
+    final private function calculateSurcharge()
     {
         global $woocommerce;
 
         $percentage = 0.0165;
         $surcharge = ( $woocommerce->cart->cart_contents_total + $woocommerce->cart->shipping_total ) * $percentage;
-        // add_fee also specifies whether tax is added, currently set to 'true'
-        $woocommerce->cart->add_fee( 'CO2 compensatie', $surcharge, true, '' );
 
+        return $surcharge;
+    }
+
+    public function my_custom_cart_field($cart)
+    {
+        global $woocommerce;
+
+        if ( isset( $_POST['post_data'] ) ) {
+            parse_str( $_POST['post_data'], $post_data );
+        } else {
+            $post_data = $_POST;
+        }
+        echo '<div id="my_custom_checkout_field"><h2>'.__('CO2 Compensatie').'</h2>';
+
+        woocommerce_form_field('co2-ok', array(
+            'type' => 'checkbox',
+            'id' => 'co2-ok-cart',
+            'class' => array(
+                'co2-ok-cart'
+            ) ,
+            'label' => __('Ik wil de CO2 voor mijn product(en) compenseren voor €'.number_format($this->calculateSurcharge(), 2, ',', ' ')),
+            'required' => false,
+        ) ,$woocommerce->session->co2_ok);
+
+        echo '</div>
+            <script>
+                 jQuery(\'#co2-ok-cart\').click(function ()
+                 {
+                    if(jQuery(this).is(":checked"))
+                        jQuery(\'.woocommerce-cart-form\').append(\'<input type="checkbox" class="input-checkbox " name="co2-ok" id="co2-ok" checked value="1" style="display:none">\');
+                    jQuery(\'.woocommerce-cart-form\').find(\'input\').trigger("change");
+                    jQuery("[name=\'update_cart\']").trigger("click");
+                });
+            </script>';
+    }
+
+    public function my_custom_checkout_field( $checkout )
+    {
+        global $woocommerce;
+
+        echo '<div id="co2-ok"><h2>' . __('CO2 Compensatie') . '</h2>';
+        woocommerce_form_field('co2-ok', array(
+            'type' => 'checkbox',
+            'id' => 'co2-ok-checkout',
+            'class' => array(
+                'co2-ok'
+            ),
+            'label' => __('Ik wil de CO2 voor mijn product(en) compenseren voor <span id="co2-amount">€'.number_format($this->calculateSurcharge(), 2, ',', ' ')."</span>"),
+            'required' => false,
+        ), $woocommerce->session->co2_ok);
+
+        echo '</div>
+        <script>
+            jQuery(\'#co2-ok-checkout\').click(function ()
+            {
+                jQuery(\'body\').trigger(\'update_checkout\');
+            });
+        </script>';
+    }
+
+    public function woocommerce_custom_surcharge( $cart )
+    {
+        global $woocommerce;
+
+        if ( isset( $_POST['post_data'] ) ) {
+            parse_str( $_POST['post_data'], $post_data );
+        } else {
+            $post_data = $_POST;
+        }
+
+        if( $post_data['co2-ok'] == 1)
+            $woocommerce->session->co2_ok = 1;
+        else if($_POST)
+            $woocommerce->session->co2_ok = 0;
+
+        if ($woocommerce->session->co2_ok == 1)
+        {
+            $woocommerce->cart->add_fee( 'CO2 compensatie', $this->calculateSurcharge(), true, '' );
+        }
     }
 }
 $co2okPlugin = new Co2ok_Plugin();
+register_activation_hook( __FILE__, array( 'Co2ok_Plugin', 'Activated' ) );
+register_deactivation_hook( __FILE__, array( 'Co2ok_Plugin', 'Deactivated' ) );
 ?>
