e40eee134b45fc5d10b8f0821f23da7e4ade5f29
Chris Fuller <fuller.c.r.89@gmail.com>
2017-08-28 05:40:09 -0400

    version+ in description

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -6,7 +6,7 @@
  *
  * Plugin URI: https://github.com/Mil0dV/co2ok-plugin-woocommerce
  * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
- * Version: 0.2.0
+ * Version: 0.2.1
  *         (Remember to change the VERSION constant, below, as well!)
  * Author:
  * Chris Fuller,
