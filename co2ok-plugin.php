946d7e998080ce54c5a1f526d9dd01fb3534ba00
Chris Fuller <fuller.c.r.89@gmail.com>
2017-08-28 17:02:07 -0400

    fixed youtube hover movie for checkout page..

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -114,7 +114,8 @@ class Co2ok_Plugin
             'label' =>
                 __('<span class="co2_label"> Make'.$this->helperComponent->RenderImage('images/logo.svg',null,'co2-ok-logo')
                 .' for €'.number_format($this->calculateSurcharge(), 2, ',', ' ')
-                .$this->helperComponent->RenderImage('images/info.gif',null,'co2-ok-info').'</span>'
+                .$this->helperComponent->RenderImage('images/info.gif','co2-ok-info','co2-ok-info')
+                    .'</span><div class="youtubebox_container" style="width:1px;height:1px;overflow:hidden"> <div class="youtubebox" id="youtubebox" width="400" height="300" ></div> </div>'
             ),
             'required' => false,
         ) ,$woocommerce->session->co2_ok);
@@ -135,7 +136,7 @@ class Co2ok_Plugin
                 __('<span class="co2_label"> Make'.$this->helperComponent->RenderImage('images/logo.svg',null,'co2-ok-logo')
                     .' for €'.number_format($this->calculateSurcharge(), 2, ',', ' ')
                     .$this->helperComponent->RenderImage('images/info.gif','co2-ok-info','co2-ok-info')
-                    .'</span><div class="youtubebox_container" style="width:1px;height:1px;overflow:hidden"> <div class="youtubebox" id="youtubebox" width="400" height="300" ></div> </div> '
+                    .'</span><div class="youtubebox_container" style="width:1px;height:1px;overflow:hidden"> <div class="youtubebox" id="youtubebox" width="400" height="300" ></div> </div>'
         ),
             'required' => false,
         ), $woocommerce->session->co2_ok);
