fd8f4e896438a764385bcd84c4342757a77ca730
Milo de Vries <intensmilo@gmail.com>
2017-09-06 18:15:49 +0200

    Adds test setup with dummy test

---
+++ tests/test-co2ok.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class Co2okTest
+ *
+ * @package Co2ok_Plugin
+ */
+
+/**
+ * Co2ok test case.
+ */
+class Co2okTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_co2ok() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
