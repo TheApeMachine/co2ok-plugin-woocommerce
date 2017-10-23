8c145c14b07410f67409bc496babbf73d31177ce
Chris Fuller <fuller.c.r.89@gmail.com>
2017-10-23 06:17:47 -0400

    bump v

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -6,7 +6,7 @@
  *
  * Plugin URI: https://github.com/Mil0dV/co2ok-plugin-woocommerce
  * GitHub Plugin URI: Mil0dV/co2ok-plugin-woocommerce
- * Version: 1.0.0.9
+ * Version: 1.0.0.10
  *         (Remember to change the VERSION constant, below, as well!)
  * Author:
  * Chris Fuller,
@@ -34,7 +34,7 @@ if ( !class_exists( 'co2ok_plugin_woocommerce\Co2ok_Plugin' ) ) :
     /**
      * This plugin's version
      */
-    const VERSION = '1.0.0.9';
+    const VERSION = '1.0.0.10;
 
     static $co2okApiUrl = "https://test-api.co2ok.eco/graphql";
 
