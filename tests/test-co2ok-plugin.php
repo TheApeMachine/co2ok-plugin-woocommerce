57c84c65983bb0744cf86d873b9e135da5fdf70d
Jasper Weyne <jasperweyne@gmail.com>
2018-02-05 14:31:34 +0100

    Match for UUIDv4

--- tests/test-co2ok-plugin.php
+++ tests/test-co2ok-plugin.php
@@ -23,37 +23,35 @@ class Co2okPluginTest extends WP_UnitTestCase {
 	 */
 	final public function test_registerMerchant() {
 		// Test preconditions
-		$this->assertTrue( filter_var(Co2ok_Plugin::$co2okApiUrl, FILTER_VALIDATE_URL) );
+		$this->assertEquals( Co2ok_Plugin::$co2okApiUrl, "https://test-api.co2ok.eco/graphql" );
 		$this->assertTrue( filter_var(get_option('admin_email'), FILTER_VALIDATE_EMAIL) );
 		$this->assertTrue( isset($_SERVER['SERVER_NAME']) );
 		$this->assertFalse( get_option('co2ok_id') );
 		$this->assertFalse( get_option('co2ok_secret') );
 
-		// Setup sandbox
-		Co2ok_Plugin::$co2okApiUrl = 'localhost';
-		// TODO: local sandbox for API server
-
-		// Test random values multiple times
-		foreach (range(0, 50) as $iter) {
-			$id     = uniqid(); // TODO: specify possible configurations
-			$secret = uniqid(); // TODO: specify possible configurations
-			// TODO: set values to local sandbox
-
-			// Execute method
-			try {
-				$this->plugin->registerMerchant();
-			} catch (Exception $e) {
-				$this->assertWPError($e);
-			}
-
-			// Test postconditions
-			$this->assertEquals( $id, get_option('co2ok_id') );
-			$this->assertEquals( $id, get_option('co2ok_secret') );
-
-			// Reset postcontions
-			delete_option('co2ok_id');
-			delete_option('co2ok_secret');
+		// Execute method
+		try {
+			$this->plugin->registerMerchant();
+		} catch (Exception $e) {
+			$this->assertWPError($e);
 		}
+
+		// Test postconditions
+		$this->assertEquals( $this->isUuidv4(get_option('co2ok_id')) );
+		$this->assertEquals( $this->isUuidv4(get_option('co2ok_secret')) );
+
+		// Reset postcontions
+		delete_option('co2ok_id');
+		delete_option('co2ok_secret');
+	}
+	
+	/**
+	 * Tests whether string is a valid UUIDv4
+	 * Returns a boolean
+	 */
+	private function isUuidv4($str) {
+		$UUIDv4 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
+		return preg_match($UUIDv4, $value) === 1;
 	}
 }
 ?>
