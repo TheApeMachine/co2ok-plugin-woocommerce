408411b95228def8e4ee1484e5ffe8b0e4d46728
Quaddepo <23100@ma-web.nl>
2018-03-06 00:38:39 +0100

    added todos

--- Templates/co2ok_button_template_default.php
+++ Templates/co2ok_button_template_default.php
@@ -15,6 +15,9 @@
             <div id="checkbox">
 
             </div>
+
+              <?php // TODO: Replace text with static vars ?>
+
               <?php echo __( '<span class="make">Make</span>', 'co2ok-for-woocommerce' );
                   echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/logo.svg', 'co2ok_logo', 'co2ok_logo');
               ?>
@@ -25,7 +28,8 @@
 
 
     <span class="co2ok_payoff">
-
+      <?php // TODO: Replace text with static vars ?>
+        
         <?php
             echo  __( 'Make my purchase climate neutral', 'co2ok-for-woocommerce' );
             ?>
@@ -36,12 +40,13 @@
 
 
     <div class="co2ok_infobox_container">
+        <?php // TODO: Replace text with static vars ?>
         <p class="text-block">Tijdens productie en  vervoer van producten komen broeikasgassen vrij. </p>
         <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/fout.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
-
+        <?php // TODO: Replace text with static vars ?>
         <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/even.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
         <p class="text-block">Wij zorgen ervoor dat een even grote hoeveelheid uitstoot voorkomen wordt. </p>
-        
+
         <a target="_blank" href="http://co2ok.eco"><?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/logo.svg', 'co2ok_logo', 'co2ok_logo'); ?></a>
         <span> <a target="_blank" href="http://www.co2ok.eco/co2-compensatie"><?php
             echo  __( 'How CO&#8322; compensation works', 'co2ok-for-woocommerce' );
