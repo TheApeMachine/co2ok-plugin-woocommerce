d17819326da70a347a0605f8600bebb6775c74b4
Milo de Vries <intensmilo@gmail.com>
2018-03-09 14:23:45 +0100

    Updates translation

--- Templates/co2ok_button_template_default.php
+++ Templates/co2ok_button_template_default.php
@@ -42,17 +42,17 @@
     <div class="co2ok_infobox_container">
 
         <div class="inner-wrapper">
-          <p class="text-block greyBorder"><?php echo __('Tijdens productie en vervoer van producten komen broeikasgassen vrij.',  'co2ok-for-woocommerce' );?></p>
+          <p class="text-block greyBorder"><?php echo __('During manufacturing and shipping of products, greenhouse gases are emitted',  'co2ok-for-woocommerce' );?></p>
           <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/fout.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
         </div>
 
         <div class="inner-wrapper">
           <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/even.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
-          <p class="text-block greyBorder"><?php echo __('Wij zorgen ervoor dat evenveel uitstoot voorkomen wordt.',  'co2ok-for-woocommerce' );?></p>
+          <p class="text-block greyBorder"><?php echo __('We make sure the same amount of emissions is prevented',  'co2ok-for-woocommerce' );?></p>
         </div>
 
         <div class="inner-wrapper">
-          <p class="text-block"><?php echo __('Zo is je aankoop klimaatneutraal!',  'co2ok-for-woocommerce' );?></p>
+          <p class="text-block"><?php echo __('This way, your purchase is climate neutral!',  'co2ok-for-woocommerce' );?></p>
         </div>
 
         <a target="_blank" href="http://co2ok.eco"><?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/logo.svg', 'co2ok_logo', 'co2ok_logo'); ?></a>
