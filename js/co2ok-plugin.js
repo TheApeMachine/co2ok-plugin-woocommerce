e95c61606e999e3ffdb7fd7cfbaa0259b625d7d2
Quaddepo <23100@ma-web.nl>
2018-03-22 10:54:44 +0100

    changed logo imags from svg to png

--- js/co2ok-plugin.js
+++ js/co2ok-plugin.js
@@ -32,13 +32,13 @@ var Co2ok_JS = function ()
                 });
 
                 if (!(jQuery('#co2ok_cart').is(":checked"))) {
-                      jQuery("#co2ok_logo").attr("src", image_url + '/logo.svg');
+                      jQuery("#co2ok_logo").attr("src", image_url + '/logo.png');
                 }
 
                 if(jQuery('#co2ok_cart').is(":checked"))
                 {
                     // change image source
-                    jQuery("#co2ok_logo").attr("src", image_url + '/logo_wit.svg');
+                    jQuery("#co2ok_logo").attr("src", image_url + '/logo_wit.png');
                 }
 
         },
@@ -95,13 +95,13 @@ var Co2ok_JS = function ()
                 //
                 //alert("mofo");
                 if (!(jQuery(this).is(":checked"))) {
-                      jQuery("#co2ok_logo").attr("src", image_url + '/logo.svg');
+                      jQuery("#co2ok_logo").attr("src", image_url + '/logo.png');
                 }
 
                 if(jQuery(this).is(":checked"))
                 {
                     // change image source
-                    jQuery("#co2ok_logo").attr("src", image_url + '/logo_wit.svg');
+                    jQuery("#co2ok_logo").attr("src", image_url + '/logo_wit.png');
 
                     jQuery('.co2ok_checkbox_container').addClass('selected');
                     jQuery('.co2ok_checkbox_container').removeClass('unselected');
