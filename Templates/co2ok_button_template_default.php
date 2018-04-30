e99cbf8161c8a580c89adeae4f04e7aaa3399b66
Michielv10hoven <37332723+Michielv10hoven@users.noreply.github.com>
2018-04-30 13:22:56 +0200

    Test put popup infobox into "p" span
    Checking what happens if I put popup infobox into the "p" span

--- Templates/co2ok_button_template_default.php
+++ Templates/co2ok_button_template_default.php
@@ -32,11 +32,8 @@
             ?>
         <span id="p">
             <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/info.svg', 'co2ok_info', 'co2ok_info'); ?>
-        </span>
-    </span>
-
-
-    <div class="co2ok_infobox_container co2ok-popper">
+            
+            <div class="co2ok_infobox_container co2ok-popper">
 
         <div class="inner-wrapper">
           <p class="text-block greyBorder"><?php echo __('During manufacturing and shipping of products, greenhouse gases are emitted',  'co2ok-for-woocommerce' );?></p>
@@ -57,6 +54,12 @@
             echo  __( 'How CO&#8322; compensation works', 'co2ok-for-woocommerce' );
             ?></a> </span>
     </div>
+
+        </span>
+    </span>
+
+
+    
 
 
 </div>
