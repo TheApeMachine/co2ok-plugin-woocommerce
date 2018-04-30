92a68f8f59f3b6daf9c64aeaea517a478af4d208
Michielv10hoven <37332723+Michielv10hoven@users.noreply.github.com>
2018-04-30 14:43:44 +0200

    Check if html html is rendered properly in template
    Checking if the order of blocks is changed it gets rendered in the source on the test website

--- Templates/co2ok_button_template_default.php
+++ Templates/co2ok_button_template_default.php
@@ -33,30 +33,33 @@
         <span id="p">
             <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/info.svg', 'co2ok_info', 'co2ok_info'); ?>
         </span>
+
+<div class="co2ok_infobox_container co2ok-popper">
+
+<div class="inner-wrapper">
+  <p class="text-block greyBorder"><?php echo __('During manufacturing and shipping of products, greenhouse gases are emitted',  'co2ok-for-woocommerce' );?></p>
+  <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/fout.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
+</div>
+
+<div class="inner-wrapper">
+  <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/even.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
+  <p class="text-block greyBorder"><?php echo __('We make sure the same amount of emissions is prevented',  'co2ok-for-woocommerce' );?></p>
+</div>
+
+<div class="inner-wrapper">
+  <p class="text-block"><?php echo __('This way, your purchase is climate neutral!',  'co2ok-for-woocommerce' );?></p>
+</div>
+
+<a class="hover-link" target="_blank" href="http://co2ok.eco"><?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/logo.svg', 'co2ok_logo hover-link', 'co2ok_logo'); ?></a>
+<span> <a  class="hover-link" target="_blank" href="http://www.co2ok.eco/co2-compensatie"><?php
+    echo  __( 'How CO&#8322; compensation works', 'co2ok-for-woocommerce' );
+    ?></a> </span>
+</div>
+
     </span>
 
 
-    <div class="co2ok_infobox_container co2ok-popper">
-
-        <div class="inner-wrapper">
-          <p class="text-block greyBorder"><?php echo __('During manufacturing and shipping of products, greenhouse gases are emitted',  'co2ok-for-woocommerce' );?></p>
-          <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/fout.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
-        </div>
-
-        <div class="inner-wrapper">
-          <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/even.svg', 'svg-img', '  co2ok_info_hover_image'); ?>
-          <p class="text-block greyBorder"><?php echo __('We make sure the same amount of emissions is prevented',  'co2ok-for-woocommerce' );?></p>
-        </div>
-
-        <div class="inner-wrapper">
-          <p class="text-block"><?php echo __('This way, your purchase is climate neutral!',  'co2ok-for-woocommerce' );?></p>
-        </div>
-
-        <a class="hover-link" target="_blank" href="http://co2ok.eco"><?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderImage('images/logo.svg', 'co2ok_logo hover-link', 'co2ok_logo'); ?></a>
-        <span> <a  class="hover-link" target="_blank" href="http://www.co2ok.eco/co2-compensatie"><?php
-            echo  __( 'How CO&#8322; compensation works', 'co2ok-for-woocommerce' );
-            ?></a> </span>
-    </div>
+    
 
 
 </div>
