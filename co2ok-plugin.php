17c48bf7a4a7d623fe3845bcab2fd8f5f447d5c0
Milo de Vries <intensmilo@gmail.com>
2018-06-20 16:00:50 +0200

    Fixes remaining issues with WC2.1

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -443,11 +443,13 @@ if ( !class_exists( 'co2ok_plugin_woocommerce\Co2ok_Plugin' ) ) :
         switch ($new_status) {
             case "processing":
                 // $order = get_order($order_id);
-                $order = new WC_Order($order_id);
+                $order = new \WC_Order($order_id);
                 $fees = $order->get_fees();
 
                 foreach ($fees as $fee) {
-                    if ($fee->get_name() == __( 'CO2 compensation (Inc. VAT)', 'co2ok-for-woocommerce' )) {
+                    $feeName = method_exists($fee, 'get_name') ? $fee->get_name() : $fee->name;
+
+                    if ($feeName == __( 'CO2 compensation (Inc. VAT)', 'co2ok-for-woocommerce' )) {
                         // The user did opt for co2 compensation
                         $this->co2ok_storeTransaction($order_id);
                     }
