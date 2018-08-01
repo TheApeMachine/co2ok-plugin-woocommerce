949dfa928313f59204c04a2d0a19831d64b8f456
Michielv10hoven <m.v.tienhoven@gmail.com>
2018-08-01 14:23:45 +0200

    Added co2ok freemius logo with freemius logo customization and added freemius opt in text customization
    Logo: new binary (jpg)
    New function: Freemius opt-in Text Customization
    New filter for text customization
    
    New function: Freemius opt-in Icon Customization
    New filter for icon customization

--- co2ok-plugin.php
+++ co2ok-plugin.php
@@ -61,6 +61,35 @@ co2okfreemius();
 // Signal that SDK was initiated.
 do_action( 'co2okfreemius_loaded' );
 
+// Freemius opt-in Text Customization
+function my_fs_custom_connect_message_on_update(
+    $message,
+    $user_first_name,
+    $product_title,
+    $user_login,
+    $site_link,
+    $freemius_link
+) {
+    return sprintf(
+        __( 'Hey %1$s', 'my-text-domain' ) . ',<br>' .
+        __( 'Please help us improve %2$s! If you opt-in, some data about your usage of %2$s will be sent to %5$s. If you skip this, that\'s okay! %2$s will still work just fine.', 'my-text-domain' ),
+        $user_first_name,
+        '<b>' . $product_title . '</b>',
+        '<b>' . $user_login . '</b>',
+        $site_link,
+        $freemius_link
+    );
+}
+ 
+co2okfreemius()->add_filter('connect_message_on_update', 'my_fs_custom_connect_message_on_update', 10, 6);
+
+// Freemius opt-in Icon Customization
+function my_fs_custom_icon() {
+    return dirname( __FILE__ ) . '/images/co2ok_freemius_logo.{png|jpg|gif|svg}';
+}
+ 
+co2okfreemius()->add_filter( 'plugin_icon' , 'my_fs_custom_icon' );
+
 /**
   * Only activate plugin on cart and checkout page
   */
