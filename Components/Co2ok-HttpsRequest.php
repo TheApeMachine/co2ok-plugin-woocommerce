4f3935c4eca09ded8f13b928de3bb5c8d4e031d3
Milo de Vries <intensmilo@gmail.com>
2019-07-24 17:40:19 +0200

    Adds retry to graphql client

--- Components/Co2ok-HttpsRequest.php
+++ Components/Co2ok-HttpsRequest.php
@@ -14,6 +14,25 @@ class Co2ok_HttpsRequest
     {
             $http = _wp_http_get_object();
 
-        return $http->post(  $this->url.'?query='.urlencode($body), array( ) );
+            $tries = 4;
+            $sleep = 1; 
+            do {
+                $response = $http->post(  $this->url.'?query='.urlencode($body), array( ) );
+                if (200 == wp_remote_retrieve_response_code($response)) {
+                    // Looks good!
+                    break;
+                } else {
+                    // printf("\nInvalid response from API, sleeping %d seconds and trying again...\n", intval($sleep));
+                    sleep($sleep);
+                    ++$sleep;
+                }
+            } while (--$tries);
+            if (200 != wp_remote_retrieve_response_code($response)) {
+                // This would be the place to do remote logging
+                // not exit though: calling functions expect the JSON response
+                // exit('Could not connect to API server.');
+            }
+
+        return $response;
     }   
 }
