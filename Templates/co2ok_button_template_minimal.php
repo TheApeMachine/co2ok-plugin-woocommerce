1552d9facca64657b8863da6026d5e25d2014c29
Milo de Vries <intensmilo@gmail.com>
2019-12-30 11:28:52 +0100

    Fixes iOS full screen 'gifs'

--- Templates/co2ok_button_template_minimal.php
+++ Templates/co2ok_button_template_minimal.php
@@ -101,7 +101,7 @@
     <?php if ( $co2ok_gif_feature == 'on' ): ?>
     <div class="co2ok_videoRewardBox_container" id="videoRewardBox-view">
 
-        <video width="320" height="240" autoplay id="co2ok_videoReward">
+        <video width="320" height="240" autoplay id="co2ok_videoReward" playsinline>
         <?php echo co2ok_plugin_woocommerce\Components\Co2ok_HelperComponent::RenderRandomizedVideo(); ?>
           Your browser does not support the video tag.
         </video>
