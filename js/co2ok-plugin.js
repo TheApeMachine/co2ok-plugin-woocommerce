89b6fde75918140b8883ee6c697ae194e9943615
ghostyfied <caglois@gmail.com>
2020-09-25 15:58:44 +0200

    Fix deprecated jQuery - rebinding bug

--- js/co2ok-plugin.js
+++ js/co2ok-plugin.js
@@ -666,7 +666,7 @@ var Co2ok_JS = function ()
           // this ofc breaks our bindings.
           jQuery( document.body ).on( 'updated_cart_totals', function(){
             // detect if elements are bound:
-            if (! jQuery(".co2ok_checkbox_container").data('events')){
+            if (!jQuery._data(jQuery('.co2ok_checkbox_container').get(0), "events")){
               console.log('Rebinding CO2ok')
               Co2ok_JS().RegisterBindings()
             }
